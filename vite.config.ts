a20f8a7cf4a3b813946d708edd7b6494ebd985d2
Federico Zivolo <5382443+FezVrasta@users.noreply.github.com>
2026-01-14 14:15:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/cafe/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
